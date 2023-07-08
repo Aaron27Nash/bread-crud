37c31123273825c7629fa16bc2367982cce839ee
Aaron Nash <aaron27nash@gmail.com>
2023-07-08 13:14:38 -0400

    added html rendering

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require('dotenv').config()
 const breadController = require('./controllers/bread')
 
 const app = express()
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //routes
 app.use('/breads', breadController)
 
